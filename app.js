2b6cc772183c62ed109e7785526d6e0fdf06d682
Jeremy <jeremy.kasala@gmail.com>
2015-02-19 17:30:22 -0500

    - created config module for db connection

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var dbconfig = require('./dbconfig');
 
 // database
 var mongo = require('mongoskin');
-var db = mongo.db("mongodb://localhost:27017/testimonial", {native_parser:true});
+var db = mongo.db(dbconfig.connection, {native_parser:true});
 
 var routes = require('./routes/index');
 var testimonials = require('./routes/testimonials');
