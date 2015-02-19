ae40cf7e0b930e000ba8ee9bb9d0bafb9d56ac8d
Jeremy <jeremy.kasala@gmail.com>
2015-02-19 18:06:35 -0500

    - changed mongodb URI to production URI

--- dbconfig.js
+++ dbconfig.js
@@ -1,3 +1,3 @@
-var connection = "mongodb://localhost:27017/testimonial";
+var connection = "mongodb://heroku_app34129423:7nin1pes3hcs4fqdsrasim1nls@ds045031.mongolab.com:45031/heroku_app34129423";
 
 module.exports.connection = connection;
